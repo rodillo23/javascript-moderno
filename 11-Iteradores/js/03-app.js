d97c417c97de55c095f3fd5d887e1a2780f6697e
Rodolfo Figueroa <ficr23@hotmail.com>
2024-01-19 23:03:21 -0600

    Ejercicio Fizz Buzz

---
+++ 11-Iteradores/js/03-app.js
@@ -0,0 +1,17 @@
+//fizz buzz
+// multiplo de 3 fizz
+// multiplo de 5 buzz   
+// de ambos fizz buzz
+
+for (let i = 0; i < 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log(`${i} - Fizz Buzz`);
+    }else if(i % 3 === 0){
+        console.log(`${i} - Fizz`);
+    }else if(i % 5 === 0){
+        console.log(`${i} - Buzz`);
+    }else{
+        console.log(`${i}`);
+    }
+    
+}
