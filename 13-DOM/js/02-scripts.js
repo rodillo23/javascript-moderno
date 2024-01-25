49a564c5c088d1d9f4ef6b5b866d112fd62477cd
Rodolfo Figueroa <ficr23@hotmail.com>
2024-01-24 23:09:18 -0600

    DOM, acceder a elementos con document, getElementsByClassName, getElementById

---
+++ 13-DOM/js/02-scripts.js
@@ -0,0 +1,10 @@
+//getElementsByClassName -> Seleccionar elementos por su clase
+
+const header = document.getElementsByClassName('header');
+console.log(header);
+
+const hero = document.getElementsByClassName('hero')
+console.log(hero);
+
+const contenedores = document.getElementsByClassName('contenedor')
+console.log(contenedores);
---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,4 @@
+//getElementById -> Seleccionar elemento por su id, retorna el primer elemento
+
+const formulario = document.getElementById('formulario')
+console.log(formulario);
