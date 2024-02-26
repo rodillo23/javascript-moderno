8f3e5977256016f24325a48f6eea93c5d6364263
Rodolfo Figueroa <ficr23@hotmail.com>
2024-02-25 23:11:34 -0600

    Proyecto Filtrar en base a compos - Ya funcionan los filtros de marca, año, precio maximo y minimo

---
+++ 17-PROYECTO-Buscador/js/app.js
@@ -0,0 +1,144 @@
+//variables
+const marca = document.querySelector('#marca');
+const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+const resultado = document.querySelector('#resultado');
+
+const max = new Date().getFullYear();
+const min = max - 10;
+
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+};
+
+//listeners
+document.addEventListener('DOMContentLoaded', function(){
+    mostrarAutos(autos);
+
+    llenarSelect();
+});
+
+marca.addEventListener('change', (e) => {
+    datosBusqueda.marca = e.target.value;
+    filtrarAuto();
+});
+
+year.addEventListener('change', (e) => {
+    datosBusqueda.year = parseInt(e.target.value);
+    filtrarAuto();
+});
+
+minimo.addEventListener('change', (e) => {
+    datosBusqueda.minimo = e.target.value;
+    filtrarAuto();
+});
+
+maximo.addEventListener('change', (e) => {
+    datosBusqueda.maximo = e.target.value;
+    filtrarAuto();
+});
+
+puertas.addEventListener('change', (e) => {
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', (e) => {
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener('change', (e) => {
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
+})
+
+
+//funciones
+
+function mostrarAutos(autos){
+    
+    limpiarHTML();
+
+    autos.forEach( auto => {
+
+        const {marca, modelo, year, puertas, transmision, precio, color} = auto;
+        const autoHTML = document.createElement('p');
+
+        autoHTML.textContent = `
+            ${marca} ${modelo} - ${year} - ${puertas} puertas - transminsion ${transmision} - 
+            precio:$ ${precio} - color: ${color}
+        `;
+
+        resultado.appendChild(autoHTML);
+    })
+}
+
+//generar los años del select
+function llenarSelect(){
+
+    for (let y = max; y >= min; y--) {
+        const opcion = document.createElement('option');
+        opcion.value = y;
+        opcion.textContent = y;
+        year.appendChild(opcion);
+        
+    };
+}
+
+//funcion que filtra en base a la busqueda
+function filtrarAuto(){
+
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo);
+    mostrarAutos(resultado);
+};
+
+function filtrarMarca(auto){
+
+    if (datosBusqueda.marca) {
+        return auto.marca === datosBusqueda.marca;
+    }
+
+    return auto;
+};
+
+function filtrarYear(auto){
+
+    if(datosBusqueda.year){
+        return auto.year === datosBusqueda.year;
+    }
+
+    return auto;
+};
+
+function filtrarMinimo(auto){
+
+    if (datosBusqueda.minimo) {
+        return auto.precio >= datosBusqueda.minimo;
+    }
+    return auto;
+};
+
+function filtrarMaximo(auto){
+
+    if (datosBusqueda.maximo) {
+        return auto.precio <= datosBusqueda.maximo
+    }
+
+    return auto;
+}
+
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild   (resultado.firstChild);
+    }
+};
