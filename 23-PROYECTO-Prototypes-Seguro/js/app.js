fdb9036be8f6840bd1f1ffeab9ec5aa1dd5a3074
Rodolfo Figueroa <ficr23@hotmail.com>
2024-03-08 23:26:20 -0600

    Proyecto Cotizar seguro- logica para cotizar en base a parametros terminada

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -4,6 +4,52 @@ function Seguro(marca, year, tipo){
     this.tipo = tipo;
 };
 
+Seguro.prototype.cotizarSeguro = function(){
+    /*
+        1 = Americano 1.15
+        2 = Asiatico 1.05
+        3 = Europeo 1.35 
+    */
+
+    let cantidad;
+    const base = 2000;
+
+    switch(this.marca) {
+        case '1':
+            cantidad = base * 1.15;
+            break;
+
+        case '2':
+            cantidad = base * 1.05;
+            break;
+
+        case '3':
+            cantidad = base * 1.35;
+            break;
+
+        default: 
+            break;
+    }
+
+    //leer el año
+    const diferencia = new Date().getFullYear() - this.year;
+    //cada año que la diferencia es mayor, el costo va a reducirse un 3%
+    cantidad -= ((diferencia * 3) * cantidad) / 100;
+
+    /*
+        Si el seguro es basico se multiplica por un 30% mas
+        Si el seguro es completo se multiplica por un 50% mas
+    */
+
+    if (this.tipo === 'basico') {
+        cantidad *= 1.30
+    }else{
+        cantidad *= 1.50
+    }
+    
+    return cantidad;
+}
+
 function UI(){};
 
 UI.prototype.llenarSelect = function(){
@@ -21,6 +67,28 @@ UI.prototype.llenarSelect = function(){
     };
 
 };
+
+UI.prototype.mostrarMensaje = function(mensaje, tipo){
+
+    const div = document.createElement('div');
+
+    if (tipo === 'error') {
+        div.classList.add('error');
+    }else{
+        div.classList.add('correcto');
+    };
+
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
+
+}
 
 const ui = new UI();
 
@@ -43,11 +111,19 @@ function validarFormulario(e){
     //año
     const year = document.querySelector('#year').value;
     //tipo
-    const tipo = document.querySelector('input[name="tipo"]').value;
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
+    //validacion y mostrar mensaje
     if (marca === '' || year === '' || tipo === '') {
-        console.log('No pasa la validación');
-    }else{
-        console.log('Si pasa la validación!');
-    }   
+        ui.mostrarMensaje('Todos los campos son obligatorios.', 'error');
+        return;
+    };
+
+    ui.mostrarMensaje('Cotizando..', 'correcto');
+
+    //Instanciar el seguro
+    const seguro = new Seguro(marca, year, tipo);
+
+    //utilizar el prototype que va a cotizar
+    seguro.cotizarSeguro();
 }
