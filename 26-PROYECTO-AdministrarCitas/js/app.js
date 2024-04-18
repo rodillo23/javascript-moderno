86deaf5539ef6439f3bad72041148d6b07b88523
Rodolfo Figueroa <ficr23@hotmail.com>
2024-04-17 23:27:08 -0600

    Seccion 26 Administrador de citas - se agrego la lfuncionalidad de eliminar cita desde el boton de eliminar

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -78,11 +78,21 @@ class AdminCitas{
         this.mostrar();
     }
 
+    eliminar(id){
+        this.citas = this.citas.filter( cita => cita.id != id);
+        this.mostrar();
+    }
+
     mostrar(){
         //limpiar HTML previo
         while(contenedorCitas.firstChild){
             contenedorCitas.removeChild(contenedorCitas.firstChild);
         };
+
+        if (this.citas.length == 0) {
+            contenedorCitas.innerHTML = '<p class="text-xl mt-5 mb-10 text-center">No Hay Pacientes</p>';
+            return
+        }
 
         //generar citas 
         this.citas.forEach( cita => {
@@ -115,11 +125,13 @@ class AdminCitas{
 
             //Toma una copia de un objeto
             //const clone = structuredClone(cita)
-            btnEditar.onclick = () => cargarEdicion(cita);
+            btnEditar.onclick = () => cargarEdicion(cita); //-> Event handler 
 
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
             btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+
+            btnEliminar.onclick = () => this.eliminar(cita.id);
 
             const contenedorBotones = document.createElement('DIV');
             contenedorBotones.classList.add('flex', 'justify-between', 'mt-5');
